e69ea3a6bbedb860385dc8c744865ebcd30fff7a
nils743 <nils743@w101-74.szut.uni-bremen.de>
2017-09-18 10:49:09 +0200

    - trainer erstellt

---
+++ DAO-Pattern/src/dao/pattern/trainer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao.pattern;
+
+/**
+ *
+ * @author nils743
+ */
+public class trainer {
+
+    // Attribute
+    int id;
+    String name;
+    int alter;
+    int erfahrung;
+    
+    // Konstruktor
+    public trainer() {}
+   
+    // Getter und Setter
+    void setId(int id) {
+    this.id = id;
+    }
+    
+}
