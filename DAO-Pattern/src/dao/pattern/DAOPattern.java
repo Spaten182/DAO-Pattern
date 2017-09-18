cba5d52a49e9627c74ec12f8fe86a4bd8ad84606
Jaqueline Budaj <jaqueline.budaj@live.de>
2017-09-18 11:25:27 +0200

    Textausgabe gepimt

--- DAO-Pattern/src/dao/pattern/DAOPattern.java
+++ DAO-Pattern/src/dao/pattern/DAOPattern.java
@@ -27,7 +27,7 @@ public class DAOPattern extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello World! :)");
             }
         });
         
